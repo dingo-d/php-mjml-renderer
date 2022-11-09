0cd8ff62c40b2e9c9aee24668cddd08bc73da2e9
Denis Žoljom <dingo-d@users.noreply.github.com>
2022-11-09 23:12:55 +0100

    Add Node class tests

---
+++ tests/Unit/Parser/NodeTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace MadeByDenis\PhpMjmlRenderer\Tests\Unit\Parser;
+
+use MadeByDenis\PhpMjmlRenderer\Node;
+use MadeByDenis\PhpMjmlRenderer\Parser\MjmlNode;
+
+beforeEach(function () {
+    $this->node = new MjmlNode(
+		'mj-text',
+		[
+			'mj-class' => 'blue big',
+		],
+		'Hello World!',
+		false,
+		null
+	);
+});
+
+afterEach(fn() => $this->node = null);
+
+it('will return the tag', function() {
+	expect($this->node->getTag())->toBe('mj-text');
+});
+
+it('will return boolean if the tag is self-closing or not', function() {
+	expect($this->node->isSelfClosing())->toBeFalse();
+});
+
+it('will return the array of attributes', function() {
+	$attributes = $this->node->getAttributes();
+	expect($attributes)
+		->toBeArray()
+		->toHaveKey('mj-class')
+		->and($attributes['mj-class'])->toBe('blue big');
+});
+
+it('will return the content of the node', function() {
+	expect($this->node->getInnerContent())->toBe('Hello World!');
+});
+
+it('will return attribute value', function() {
+	expect($this->node->getAttributeValue('mj-class'))->toBe('blue big');
+});
+
+it('will return null if there are no children nodes', function() {
+	expect($this->node->getChildren())->toBeNull();
+});
+
+it('will return the children nodes', function() {
+	$node = new MjmlNode(
+		'mj-column',
+		null,
+		null,
+		false,
+		[
+			new MjmlNode(
+				'mj-text',
+				[
+					'mj-class' => 'blue big'
+				],
+				'Hello World!',
+				false,
+				null,
+			),
+		]
+	);
+
+	expect($node->getChildren())
+		->toBeArray()
+		->and($node->getChildren()[0])
+		->toBeInstanceOf(Node::class);
+});
