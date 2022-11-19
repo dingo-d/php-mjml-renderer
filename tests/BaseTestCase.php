57283633b9472b89a18c26fe3c69d00cb6b099f8
Denis Žoljom <denis.zoljom@gmail.com>
2022-11-19 12:52:25 +0100

    Update base test class

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -2,6 +2,7 @@
 
 namespace MadeByDenis\PhpMjmlRenderer\Tests;
 
+use MadeByDenis\PhpMjmlRenderer\Elements\Element;
 use MadeByDenis\PhpMjmlRenderer\Parser\MjmlNode;
 use MadeByDenis\PhpMjmlRenderer\Parser\MjmlParser;
 use MadeByDenis\PhpMjmlRenderer\Renderer\MjmlRenderer;
@@ -9,7 +10,8 @@ use PHPUnit\Framework\TestCase;
 
 class BaseTestCase extends TestCase
 {
-	private MjmlRenderer $renderer;
-	private MjmlParser $parser;
-	private MjmlNode $node;
+	private ?MjmlRenderer $renderer;
+	private ?MjmlParser $parser;
+	private ?MjmlNode $node;
+	private ?Element $element;
 }
